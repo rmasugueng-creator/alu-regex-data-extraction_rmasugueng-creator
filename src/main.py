542f7c36d6582dfbccead057c5c803ca8d1f94eb
rmasugueng-creator <r.masugueng@alustudent.com>
2026-09-09 12:56:43 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
+import re
+import json
 
+INPUT_FILE = "input/raw-text.txt"
+OUTPUT_FILE = "output/sample-output.json"
+
+
+def main():
+    with open(INPUT_FILE, "r", encoding="utf-8") as file:
+        text = file.read()
+
+    print(text)
+
+
+if __name__ == "__main__":
+    main()
